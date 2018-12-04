56da876550cd44e5bbfdcbd760c16e98ab1d6d1e
Ravi Kant Shukla <ravikantshukla91@gmail.com>
2018-12-04 23:27:56 +0530

    student-rest-services

---
+++ src/main/java/com/demo/springboot/studentrestservices/StudentRestServicesApplication.java
@@ -0,0 +1,12 @@
+package com.demo.springboot.studentrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRestServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/demo/springboot/studentrestservices/StudentRestServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.springboot.studentrestservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentRestServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
